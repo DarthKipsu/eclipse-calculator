9ffb00f8b3b5ef27a27a3d8ce09da279c2126c97
Verna Koskinen <darth.kipsu@gmail.com>
2015-01-13 19:50:41 +0200

    add odds to hit with one dice

--- src/eclipse/core/internal.clj
+++ src/eclipse/core/internal.clj
@@ -3,6 +3,9 @@
 (defn- is-opponent-for? [state]
   (fn [target] (not= state (:state target))))
 
+(defn- component [ship component]
+  (get-in ship [:components component]))
+
 (defn targets-for
   "Takes a string presentation of status and a list containing map presentations 
   of ships, filters through the list and returns those with unmatching states.
@@ -15,5 +18,14 @@
   weaponry installed. Returns true if it has."
   [ship]
   (or
-    (< 0 (get-in ship [:components :dice1HPmissile]))
-    (< 0 (get-in ship [:components :dice2HPmissile]))))
+    (< 0 (component ship :dice1HPmissile))
+    (< 0 (component ship :dice2HPmissile))))
+
+(defn hit-once-odds
+  "Takes two map presentations of ships and returns the odds for the first one
+  of them to hit the second with one dice."
+  [ship-a ship-d]
+  (let [numerator (+ 1 (component ship-a :computer) (component ship-d :shield))]
+    (if (> numerator 1)
+      (if (< numerator 6) (/ numerator 6) (/ 5 6))
+      (/ 1 6))))
--- test/eclipse/core/internal_test.clj
+++ test/eclipse/core/internal_test.clj
@@ -5,22 +5,30 @@
 (def att-interceptor
   {:state "attacker",
    :components {:dice1HPmissile 0,
-                :dice2HPmissile 1}})
+                :dice2HPmissile 1,
+                :computer 0,
+                :shield 0}})
 
 (def att-dreadnought
   {:state "attacker",
    :components {:dice1HPmissile 1,
-                :dice2HPmissile 0}})
+                :dice2HPmissile 0,
+                :computer 5,
+                :shield -5}})
 
 (def def-interceptor
   {:state "defender",
    :components {:dice1HPmissile 0,
-                :dice2HPmissile 0}})
+                :dice2HPmissile 0,
+                :computer 0,
+                :shield 0}})
 
 (def def-cruiser
   {:state "attacker",
    :components {:dice1HPmissile 1,
-                :dice2HPmissile 1}})
+                :dice2HPmissile 1,
+                :computer 2,
+                :shield -2}})
 
 (deftest- filter-and-return-targets-test
           (is (= 1 (count (targets-for "attacker" [def-interceptor att-interceptor]))))
@@ -33,3 +41,9 @@
           (is (has-missiles? att-dreadnought))
           (is (has-missiles? def-cruiser))
           (is (not (has-missiles? def-interceptor))))
+
+(deftest- get-hit-probabilities
+          (is (= (/ 1 6) (hit-once-odds att-interceptor def-interceptor)))
+          (is (= (/ 5 6) (hit-once-odds att-dreadnought def-interceptor)))
+          (is (= (/ 4 6) (hit-once-odds att-dreadnought def-cruiser)))
+          (is (= (/ 1 6) (hit-once-odds def-interceptor att-dreadnought))))
