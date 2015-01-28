abe0868fbbad39db806f6dc5ac29a5251d602da3
Verna Koskinen <darth.kipsu@gmail.com>
2015-01-28 12:47:31 +0200

    add base for win probabilities function, unfinished

--- src/eclipse/core/core.clj
+++ src/eclipse/core/core.clj
@@ -7,6 +7,14 @@
 (defn reform-ships
   "Takes a vector containing ships involved in the battle, reforms them to be
   usable for probability calculations and returns a new array containing the 
-  reformed ships. Unfinished."
+  reformed ships."
   [ships]
   (mapv-indexed reform-single-ship ships))
+
+(defn win-probabilities
+  "Takes a vector containig map presentations of ships and returns a map contai-
+  ning win probabilities for both sides as well as vector containing probabilities
+  for each ship to be alive after the match."
+  [ships]
+  (let [ships-after-missiles (missiles-round ships)]
+    (cannons-round ships-after-missiles 3)))
--- test/eclipse/core/core_test.clj
+++ test/eclipse/core/core_test.clj
@@ -7,3 +7,11 @@
 (facts "vector reforming"
   (fact "transforms the vector received from ui to a more usable form"
         (reform-ships [json-01 json-02]) => [reform-01 reform-02]))
+
+(facts "final probabilities"
+  (fact "returns a map containing win probabilities for both sides and alive odds
+        for all ships in battle after missiles and 3 cannon rounds."
+        (win-probabilities [cannon-04 cannon-05]) =future=>
+          {"attacker" 2/3,
+           "defender" 1/3,
+           "alive-odds" ["46.5" "36,8"]}))
