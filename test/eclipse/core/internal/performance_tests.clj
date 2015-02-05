8083d4f0d24b8b721fcaad5ee7b7f2ab3c862e96
Verna Koskinen <darth.kipsu@gmail.com>
2015-02-05 15:57:05 +0200

    performance tests for my-some

--- test/eclipse/core/internal/performance_tests.clj
+++ test/eclipse/core/internal/performance_tests.clj
@@ -3,19 +3,38 @@
             [eclipse.core.internal.my-clj-function-implementations :refer :all])
   (:use midje.sweet))
 
-(def vec1000 (into [] (range 1000)))
-(def vec1000inc (into [] (range 1 1001)))
-(def vec1E4 (into [] (range 10000)))
-(def vec1E4inc (into [] (range 1 10001)))
+(def map1E3 (into [] (range 1000)))
+(def map1E3inc (into [] (range 1 1001)))
+(def map1E4 (into [] (range 10000)))
+(def map1E4inc (into [] (range 1 10001)))
+(def map1E6 (into [] (range 1000000)))
+(def map1E6inc (into [] (range 1 1000001)))
 
 (facts "performance of my map function"
   (fact "comparisons between my-map and mapv"
-        (println "My map, 5 values")
+        (println "  Map, 5 values")
         (time (mapv inc [1 2 3 4 5])) => [2 3 4 5 6]
         (time (my-map inc [1 2 3 4 5])) => [2 3 4 5 6]
-        (println "My map, 1000 values")
-        (time (mapv inc vec1000)) => vec1000inc
-        (time (my-map inc vec1000)) => vec1000inc
-        (println "My map, 10 000 values")
-        (time (mapv inc vec1E4)) => vec1E4inc
-        (time (my-map inc vec1E4)) => vec1E4inc))
+        (println "  Map, 1000 values")
+        (time (mapv inc map1E3)) => map1E3inc
+        (time (my-map inc map1E3)) => map1E3inc
+        (println "  Map, 10 000 values")
+        (time (mapv inc map1E4)) => map1E4inc
+        (time (my-map inc map1E4)) => map1E4inc
+        (println "  Map, 1 000 000 values")
+        (time (mapv inc map1E6)) => map1E6inc
+        (time (my-map inc map1E6)) => map1E6inc))
+
+(def some1E3 (assoc (into [] (repeat 1000 1)) 999 2))
+(def some1E4 (assoc (into [] (repeat 10000 1)) 9999 2))
+(def some1E6 (assoc (into [] (repeat 1000000 1)) 999999 2))
+
+(facts "performance of my some function"
+  (fact "comparison between my-som and some"
+        (println "\n  Some, 5 values")
+        (time (some even? [1 3 5 7 8]))
+        (time (my-some even? [1 3 5 7 8]))
+        (println "  Some, 1000 values")
+        (time (some even? some1E3))
+        (time (my-some even? some1E3))))
+
