0e0d9ebb21a95bbc7e841ae04cac7f487837bc99
Verna Koskinen <darth.kipsu@gmail.com>
2015-02-06 21:01:58 +0200

    update dependencies and add test coverage reports

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Eclipse calculator
 
 Providing probability calculations for Eclipse board game.
+
+[![Build Status](https://travis-ci.org/DarthKipsu/eclipse-calc.svg)](https://travis-ci.org/DarthKipsu/eclipse-calc) [![Dependencies Status](http://jarkeeper.com/DarthKipsu/eclipse-calc/status.png)](http://jarkeeper.com/DarthKipsu/eclipse-calc)
--- project.clj
+++ project.clj
@@ -4,9 +4,12 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]]
-  :plugins [[lein-ring "0.8.13"]
-            [lein-kibit "0.0.8"]]
+                 [ring/ring-defaults "0.1.3"]]
+  :plugins [[lein-ring "0.9.1"]
+            [lein-kibit "0.0.8"]
+            [jonase/eastwood "0.2.1"]
+            [lein-bikeshed "0.2.0"]
+            [lein-cloverage "1.0.2"]]
   :ring {:handler eclipse.core.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
